d15f3be13e138cc93a33a5d2a88f710eae20282b
makar.peter <makar.peter98@gmail.com>
2024-10-01 08:50:10 +0200

    List view és controller

---
+++ src/main/java/inf/unideb/szfm/makar/ShoppingListController.java
@@ -0,0 +1,18 @@
+package inf.unideb.szfm.makar;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ShoppingListController {
+    @FXML
+    public void onAddElementButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onDeleteElementButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onOrderButtonClick(ActionEvent actionEvent) {
+    }
+}
